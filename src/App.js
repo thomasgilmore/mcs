5311d437adb4cb54c5cf61fc22e5ad9281ed5933
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-12-19 18:26:24 -0500

    Home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
        <NavBar />
+       <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+import McsBanner from '../MCS/Website Collateral/MCS_Banner (1).png'
+
+export default function Home() {
+  return (
+    <div className='homeDiv'>
+      <div className='homeDivText'>
+        <h1>Welcome to Moody Creature Society</h1>
+      </div>
+      <div className='homeDivPicture'>
+        <img src={McsBanner} className='McsBanner' alt="Banner" />
+      </div>
+    </div>
+  )
+}
