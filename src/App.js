f2d8b354807a202b8c232d97d7ecf5f2b0c2f804
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-12-20 10:53:45 -0500

    Swipper.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import OriginalCreatures from './components/OriginalCreatures';
+import SwiperJS from './components/Swiper';
 
 function App() {
   return (
     <div className="App">
        <NavBar />
        <Home />
+       <OriginalCreatures />
+       <SwiperJS />
     </div>
   );
 }
---
+++ src/components/Swiper.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import bucketpolo from '../MCS/*Characters/Website_Banner/mcs_character_bucketpolo.png'
+import cowboy from '../MCS/*Characters/Website_Banner/mcs_character_cowboy.png'
+import floaty from '../MCS/*Characters/Website_Banner/mcs_character_floaty.png'
+import flower from '../MCS/*Characters/Website_Banner/mcs_character_flower.png'
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Autoplay,Pagination,Navigation
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Autoplay,Pagination,Navigation]);
+
+export default function SwiperJS () {
+  return (
+    <div>
+    <Swiper allowTouchMove={false} slidesPerView={4} spaceBetween={30} centeredSlides={false} autoplay={{
+      "delay": 1000,
+      "disableOnInteraction": false
+    }} showsPagination={false} navigation={false} className="mySwiper">
+      <SwiperSlide><img src={bucketpolo} className='bucketpolo' alt="Bucket Polo" /></SwiperSlide>
+      <SwiperSlide><img src={cowboy} className='cowboy' alt="Cowboy" /></SwiperSlide>
+      <SwiperSlide><img src={floaty} className='floaty' alt="Floaty" /></SwiperSlide>
+      <SwiperSlide><img src={flower} className='flower' alt="Flower" /></SwiperSlide>
+      <SwiperSlide><img src={bucketpolo} className='bucketpolo' alt="Bucket Polo" /></SwiperSlide>
+      <SwiperSlide><img src={cowboy} className='cowboy' alt="Cowboy" /></SwiperSlide>
+      <SwiperSlide><img src={floaty} className='floaty' alt="Floaty" /></SwiperSlide>
+      <SwiperSlide><img src={flower} className='flower' alt="Flower" /></SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
