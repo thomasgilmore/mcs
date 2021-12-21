68e0ae87ab35b13b240a5b9ad0853d81cfe85bb9
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-12-21 08:40:21 -0500

    ourTeam.js

---
+++ src/components/OurTeam.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ourteam.css'
+
+export default function OurTeam() {
+  return (
+    <div id="ourTeam" className='ourTeamDiv'>
+      <h1>Our Team</h1>
+    </div>
+  )
+}
