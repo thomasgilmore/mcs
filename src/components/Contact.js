5db8dc465dc6b8970009cb9ae7909ed2e68d3389
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-12-21 08:40:56 -0500

    contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './contact.css'
+
+export default function Contact() {
+  return (
+    <div id="contact" className='contactDiv'>
+      <h1>Contact Us</h1>
+    </div>
+  )
+}
