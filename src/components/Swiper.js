3f0efd1b7e37359f885b9799ea2ff6bdfdf48d01
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-12-20 23:18:42 -0500

    added second swiper in reverse direction

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -6,6 +6,10 @@ import bucketpolo from '../MCS/*Characters/Website_Banner/mcs_character_bucketpo
 import cowboy from '../MCS/*Characters/Website_Banner/mcs_character_cowboy.png'
 import floaty from '../MCS/*Characters/Website_Banner/mcs_character_floaty.png'
 import flower from '../MCS/*Characters/Website_Banner/mcs_character_flower.png'
+import goth from '../MCS/*Characters/Website_Banner/mcs_character_goth.png'
+import lifeguard from '../MCS/*Characters/Website_Banner/mcs_character_lifeguard.png'
+import monroe from '../MCS/*Characters/Website_Banner/mcs_character_monroe.png'
+import tiger from '../MCS/*Characters/Website_Banner/mcs_tiger.png'
 
 // Import Swiper styles
 import "swiper/css";
@@ -24,10 +28,10 @@ SwiperCore.use([Autoplay,Pagination,Navigation]);
 export default function SwiperJS () {
   return (
     <div>
-    <Swiper loop={true} allowTouchMove={false} slidesPerView={4} spaceBetween={10} centeredSlides={false} autoplay={{
-      "delay": 1000,
+    <Swiper loop={true} allowTouchMove={false} slidesPerView={4} centeredSlides={false} speed={3000} autoplay={{
+      "delay": 0,
       "disableOnInteraction": false
-    }} showsPagination={false} navigation={false} className="mySwiper">
+    }} navigation={false} className="mySwiper">
       <SwiperSlide><img src={bucketpolo} className='bucketpolo' alt="Bucket Polo" /></SwiperSlide>
       <SwiperSlide><img src={cowboy} className='cowboy' alt="Cowboy" /></SwiperSlide>
       <SwiperSlide><img src={floaty} className='floaty' alt="Floaty" /></SwiperSlide>
@@ -37,6 +41,20 @@ export default function SwiperJS () {
       <SwiperSlide><img src={floaty} className='floaty' alt="Floaty" /></SwiperSlide>
       <SwiperSlide><img src={flower} className='flower' alt="Flower" /></SwiperSlide>
       </Swiper>
+      <Swiper loop={true} allowTouchMove={false} slidesPerView={4} centeredSlides={false} speed={3000} autoplay={{
+        "delay": 0,
+        "disableOnInteraction": false,
+        "reverseDirection": true,
+      }} navigation={false} className="mySwiper">
+        <SwiperSlide><img src={goth} className='goth' alt="Goth" /></SwiperSlide>
+        <SwiperSlide><img src={lifeguard} className='lifeguard' alt="Lifeguard" /></SwiperSlide>
+        <SwiperSlide><img src={monroe} className='monroe' alt="Monroe" /></SwiperSlide>
+        <SwiperSlide><img src={tiger} className='tiger' alt="Tiger" /></SwiperSlide>
+        <SwiperSlide><img src={goth} className='goth' alt="Goth" /></SwiperSlide>
+        <SwiperSlide><img src={lifeguard} className='lifeguard' alt="Lifeguard" /></SwiperSlide>
+        <SwiperSlide><img src={monroe} className='monroe' alt="Monroe" /></SwiperSlide>
+        <SwiperSlide><img src={tiger} className='tiger' alt="Tiger" /></SwiperSlide>
+        </Swiper>
     </div>
   )
 }
