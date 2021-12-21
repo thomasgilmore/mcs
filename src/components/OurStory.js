84024a76f6b9af35eef22ddf26fd852cc4a0692e
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-12-21 08:40:38 -0500

    ourStory

---
+++ src/components/OurStory.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ourstory.css';
+
+export default function OurStory() {
+  return (
+    <div id="ourStory" className='ourStoryDiv'>
+      <h1>Our Story</h1>
+    </div>
+  )
+}
