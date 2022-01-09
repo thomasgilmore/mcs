1cefbadbfa02dd55c6b94329e060a938a418d6c0
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-01-09 15:08:17 -0500

    our Story text added

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -5,6 +5,7 @@ export default function OurStory() {
   return (
     <div id="ourStory" className='ourStoryDiv'>
       <h1>The Beginning of Our Story</h1>
+      <p className='ourStoryText'>In an Alternate Universe, the Moody Creature Society are the first inhabitants of Earth to develop complex emotions. The 8,765 Society Members each contain a combination of 2 emotions, as expressed by their body and background colors, and over 200 unique traits so no 2 Members are exactly alike. The only exceptions are uniquely hand drawn embodiments of single emotions.</p>
     </div>
   )
 }
