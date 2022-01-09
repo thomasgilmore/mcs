d8594cd300e2109eb1c052206251ed7a45620988
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-01-09 14:28:58 -0500

    fixing navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,13 @@ import './navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import Logo from '../MCS/*LOGO/MCS_Icon_Bcircle.jpg';
-import McsTitle from '../MCS/*LOGO/MCS_Logo_B.jpg';
+import Logo from '../MCS/Website Collateral/MCS_Icon_Bcircle.png';
+import McsTitle from '../MCS/Website Collateral/MCS_Logo_B.png';
 
 export default function NavBar() {
   return (
     <div>
-    <Navbar collapseOnSelect bg="" expand="lg" className='navBar'>
+    <Navbar collapseOnSelect bg="" variant="dark" expand="lg" className='navBar'>
     <Container>
     <Navbar.Brand href="#home">
       <img src={Logo} className='navLogo' alt="Logo" />
@@ -18,12 +18,12 @@ export default function NavBar() {
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
       <Nav>
-        <Nav.Link className='navItem' href="#home">MCS</Nav.Link>
-        <Nav.Link className='navItem' href="#ourStory">Our Story</Nav.Link>
-        <Nav.Link className='navItem' href="#roadmap">Roadmap</Nav.Link>
-        <Nav.Link className='navItem' href="#ourTeam">Our Team</Nav.Link>
-        <Nav.Link className='navItem' href="#contact">Contact</Nav.Link>
-        <Nav.Link className='navItem' href="#faq">FAQ</Nav.Link>
+        <Nav.Link className='navItem' id="navBarItem" href="#home">MCS</Nav.Link>
+        <Nav.Link className='navItem' id="navBarItem" href="#ourStory">Our Story</Nav.Link>
+        <Nav.Link className='navItem' id="navBarItem" href="#roadmap">Roadmap</Nav.Link>
+        <Nav.Link className='navItem' id="navBarItem" href="#ourTeam">Our Team</Nav.Link>
+        <Nav.Link className='navItem' id="navBarItem" href="#contact">Contact</Nav.Link>
+        <Nav.Link className='navItem' id="navBarItem" href="#faq">FAQ</Nav.Link>
       </Nav>
     </Navbar.Collapse>
     </Container>
